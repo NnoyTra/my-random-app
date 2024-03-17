23d09be64a293b5e54107219e3fd3a044c0e62ef
Abel Morales <litogrr@gmail.com>
2024-03-17 01:58:02 -0400

    my first commit

---
+++ src/test/java/com/nnoitra/MyRandomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnoitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRandomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
